f53787b05973ee5ce4ff4c766319bc39cba5828a
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 15:24:50 -0700

    Don't support root-Project.toml extras

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,7 +5,7 @@ using PyVenvs
 
 Aqua.test_all(
     PyVenvs;
-    project_extras = true,
+    project_extras = false,  # Julia 1.0 not supported since Scratch.jl doesn't
     stale_deps = true,
     deps_compat = true,
     project_toml_formatting = true,
