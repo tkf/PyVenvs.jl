e6236bf14a196f0d16f837e934412c33b1b23c11
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 17:19:59 -0700

    WIP: add https://github.com/JuliaTesting/Aqua.jl.git#windows

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,6 +3,9 @@ module TestAqua
 import Aqua
 using PyVenvs
 
+using Pkg
+pkg"add https://github.com/JuliaTesting/Aqua.jl.git#windows"
+
 Aqua.test_all(
     PyVenvs;
     project_extras = false,  # Julia 1.0 not supported since Scratch.jl doesn't
