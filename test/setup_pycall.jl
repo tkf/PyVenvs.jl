9faf3d9830705c75fecd7ec09028a74fab85294b
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 21:49:46 -0700

    Mark test_pycall.jl as broken on Windows

--- test/setup_pycall.jl
+++ test/setup_pycall.jl
@@ -2,20 +2,8 @@ if Sys.islinux()
     @info "Do nothing in Linux; default setup should work."
 else
     # Don't use Conda.jl in Windows and macOS
-    let python
-        if Sys.iswindows()
-            python = something(
-                Sys.which("python3.exe"),
-                Sys.which("python3"),
-                Sys.which("python.exe"),
-                Sys.which("python"),
-            )
-        else
-            python = something(Sys.which("python3"), Sys.which("python"))
-        end
-        withenv("PYTHON" => python) do
-            @info "Re-building PyCall..." ENV["PYTHON"]
-            @time Pkg.build("PyCall")
-        end
+    withenv("PYTHON" => PyVenvs.Implementations.basepython()) do
+        @info "Re-building PyCall..." ENV["PYTHON"]
+        @time Pkg.build("PyCall")
     end
 end
