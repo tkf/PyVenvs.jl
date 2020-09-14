537ba04c3fd0e448fff89c4794bf586b46ecab6b
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 19:17:34 -0700

    Try python3.exe etc. in Windows

--- test/setup_pycall.jl
+++ test/setup_pycall.jl
@@ -1,8 +1,21 @@
 if Sys.islinux()
     @info "Do nothing in Linux; default setup should work."
 else
-    withenv("PYTHON" => Sys.which("python3")) do
-        @info "Re-building PyCall..." ENV["PYTHON"]
-        @time Pkg.build("PyCall")
+    # Don't use Conda.jl in Windows and macOS
+    let python
+        if Sys.iswindows()
+            python = something(
+                Sys.which("python3.exe"),
+                Sys.which("python3"),
+                Sys.which("python.exe"),
+                Sys.which("python"),
+            )
+        else
+            python = something(Sys.which("python3"), Sys.which("python"))
+        end
+        withenv("PYTHON" => python) do
+            @info "Re-building PyCall..." ENV["PYTHON"]
+            @time Pkg.build("PyCall")
+        end
     end
 end
