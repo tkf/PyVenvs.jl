2048d485689188b012273e8a7a654d8b7222ea71
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 20:24:11 -0700

    Revert "Use PyVenvs.python(PYCALL_VENV)"
    This reverts commit de308e0e1768b09c36ce2dcea4b48bdf291381a2.

--- test/setup_pycall.jl
+++ test/setup_pycall.jl
@@ -1,12 +1,21 @@
-using PyVenvs
-using PyVenvs: PYCALL_VENV
-
 if Sys.islinux()
     @info "Do nothing in Linux; default setup should work."
 else
     # Don't use Conda.jl in Windows and macOS
-    withenv("PYTHON" => PyVenvs.python(PYCALL_VENV)) do
-        @info "Re-building PyCall..." ENV["PYTHON"]
-        @time Pkg.build("PyCall")
+    let python
+        if Sys.iswindows()
+            python = something(
+                Sys.which("python3.exe"),
+                Sys.which("python3"),
+                Sys.which("python.exe"),
+                Sys.which("python"),
+            )
+        else
+            python = something(Sys.which("python3"), Sys.which("python"))
+        end
+        withenv("PYTHON" => python) do
+            @info "Re-building PyCall..." ENV["PYTHON"]
+            @time Pkg.build("PyCall")
+        end
     end
 end
