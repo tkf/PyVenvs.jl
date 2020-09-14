de308e0e1768b09c36ce2dcea4b48bdf291381a2
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-13 19:33:40 -0700

    Use PyVenvs.python(PYCALL_VENV)

--- test/setup_pycall.jl
+++ test/setup_pycall.jl
@@ -1,21 +1,12 @@
+using PyVenvs
+using PyVenvs: PYCALL_VENV
+
 if Sys.islinux()
     @info "Do nothing in Linux; default setup should work."
 else
     # Don't use Conda.jl in Windows and macOS
-    let python
-        if Sys.iswindows()
-            python = something(
-                Sys.which("python3.exe"),
-                Sys.which("python3"),
-                Sys.which("python.exe"),
-                Sys.which("python"),
-            )
-        else
-            python = something(Sys.which("python3"), Sys.which("python"))
-        end
-        withenv("PYTHON" => python) do
-            @info "Re-building PyCall..." ENV["PYTHON"]
-            @time Pkg.build("PyCall")
-        end
+    withenv("PYTHON" => PyVenvs.python(PYCALL_VENV)) do
+        @info "Re-building PyCall..." ENV["PYTHON"]
+        @time Pkg.build("PyCall")
     end
 end
